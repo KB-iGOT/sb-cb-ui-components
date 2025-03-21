7af00b09e3f3db383e16b3a036c16f088afaf06f
bharathravi-in <bharathravi.in@gmail.com>
2025-03-21 13:04:46 +0530

    changes in discussion landing page

--- sb-cb-ui-discussion-v2/library/sunbird-cb/discussion-v2/src/public-api.ts
+++ sb-cb-ui-discussion-v2/library/sunbird-cb/discussion-v2/src/public-api.ts
@@ -33,6 +33,9 @@ export * from './lib/_widgets/widget-community-search/widget-community-search.mo
 export * from './lib/_widgets/widget-topics-all/widget-topics-all.component'
 export * from './lib/_widgets/widget-topics-all/widget-topics-all.module'
 
+export * from './lib/_widgets/widget-discussionv2-landing-page/widget-discussionv2-landing-page.component'
+export * from './lib/_widgets/widget-discussionv2-landing-page/widget-discussionv2-landing-page.module'
+
 export * from './lib/_common/shortcuts/shortcuts.module'
 export * from './lib/_common/shortcuts/shortcuts.component'
 export * from './lib/_common/trending-discussions/trending-discussions.module'
