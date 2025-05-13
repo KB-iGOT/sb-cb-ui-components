8dc342600cc967bbbb014be70e1a5dba53aeac55
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2025-05-13 15:24:22 +0530

    Initial commit

---
+++ sb-cb-ui-notifications/library/sunbird-cb/notifications/src/lib/notifications.config.ts
@@ -0,0 +1,12 @@
+export const ROOT_WIDGET_CONFIG = {
+  
+  card: {
+    _type: 'cardLib',
+    cardResource: 'cardContentLib',
+    eventCardLib: 'eventCardLib'
+  },
+}
+
+export enum SakshamAI {
+ SakshamAI = 'sakshamAI'
+}
