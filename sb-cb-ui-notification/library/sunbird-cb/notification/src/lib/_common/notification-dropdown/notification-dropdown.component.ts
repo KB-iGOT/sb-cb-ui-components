8b477ec9191325dcfbd0af9053e6ddca10b23c72
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2025-05-14 19:43:14 +0530

    UI fixes

--- sb-cb-ui-notification/library/sunbird-cb/notification/src/lib/_common/notification-dropdown/notification-dropdown.component.ts
+++ sb-cb-ui-notification/library/sunbird-cb/notification/src/lib/_common/notification-dropdown/notification-dropdown.component.ts
@@ -1,49 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'sb-uin-notification-dropdown',
   templateUrl: './notification-dropdown.component.html',
   styleUrls: ['./notification-dropdown.component.scss']
 })
-export class NotificationDropdownComponent {
+export class NotificationDropdownComponent implements OnInit {
   currentTab = 'all'
+  notifications: any[] = []
   allNotifications: any[] = [
     {
       type: 'learn',
       title: '3 Courses recommended by Neha Agarwal.',
       description: 'You have a new message from your mentor.',
       timestamp: '2023-10-01T10:00:00Z',
-      read: false
+      isRead: true
     },
     {
       type: 'network',
       title: 'Anil and 5 others requested to connect.',
       description: 'View all connection request',
       timestamp: '2023-10-01T10:00:00Z',
-      read: false
+      isRead: false
     },
     {
       type: 'event',
       title: '2 New events are now live on the platform.',
       description: 'Upcoming event: Digital Learning Week. Save your spot!',
       timestamp: '2023-10-01T10:00:00Z',
-      read: false
+      isRead: false
     },
     {
       type: "discuss",
       title: "Anil and 7 others liked your post.",
       description: "View all likes on your post.",
       timestamp: '2023-10-01T10:00:00Z',
-      read: false
+      isRead: true
     }
+  ]
+
+  alerts: any[] = [
+    {
+      type: 'learn',
+      title: '3 Courses recommended by Neha Agarwal.',
+      description: 'You have a new message from your mentor.',
+      timestamp: '2023-10-01T10:00:00Z',
+      isRead: false
+    },
+    {
+      type: 'network',
+      title: 'Anil and 5 others requested to connect.',
+      description: 'View all connection request',
+      timestamp: '2023-10-01T10:00:00Z',
+      isRead: true
+    },
+    {
+      type: 'event',
+      title: '2 New events are now live on the platform.',
+      description: 'Upcoming event: Digital Learning Week. Save your spot!',
+      timestamp: '2023-10-01T10:00:00Z',
+      isRead: false
+    },
+    {
+      type: "discuss",
+      title: "Anil and 7 others liked your post.",
+      description: "View all likes on your post.",
+      timestamp: '2023-10-01T10:00:00Z',
+      isRead: true
+    },
+    {
+      type: 'event',
+      title: '8 New events are now live on the platform.',
+      description: 'Upcoming event: Digital Learning Week. Save your spot!',
+      timestamp: '2023-10-01T10:00:00Z',
+      isRead: false
+    },
   ]
 
   constructor() {
 
   }
 
+  ngOnInit() {
+    this.getNotificationsObject()
+  }
+
+  getNotificationsObject() {
+    this.notifications = this.currentTab === 'all' ? this.allNotifications : this.alerts
+  }
+
   loadNotifications(type: string) {
     this.currentTab = type
+    this.getNotificationsObject()
   }
 
   redirectToNotifications() {
