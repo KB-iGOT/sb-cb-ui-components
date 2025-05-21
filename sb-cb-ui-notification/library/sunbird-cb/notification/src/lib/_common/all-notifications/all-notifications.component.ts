bb69e207ad10d22bb0f1b3a258028b8b3d9ce6ed
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2025-05-21 12:26:14 +0530

    UI changes in all notifications

--- sb-cb-ui-notification/library/sunbird-cb/notification/src/lib/_common/all-notifications/all-notifications.component.ts
+++ sb-cb-ui-notification/library/sunbird-cb/notification/src/lib/_common/all-notifications/all-notifications.component.ts
@@ -24,31 +24,39 @@ export class AllNotificationsComponent implements OnInit {
   allNotifications: any[] = [
     {
       type: 'learn',
-      title: '3 Courses recommended by Neha Agarwal.',
-      description: 'You have a new message from your mentor.',
+      title: '12 Courses recommended by Neha Agarwal.',
+      description: 'Click here to view recommended courses.',
       timestamp: this.getTimeAgo('2025-05-15T04:51:00Z'),
-      isRead: false
+      isRead: false,
+      courses: ["do_113976864943824896111", "do_114081021046390784159", "do_11402573033635840016", "do_113882971081441280172",
+        "do_114289012599259136112", "do_114308876086607872151", "do_114277667872382976138", "do_114277928509136896171",
+        "do_11389554191936716817", "do_11431661045542092814", "do_1143018621876715521203", "do_1141256670204887041118"
+      ],
+      isExpanded: false
     },
     {
       type: 'network',
       title: 'Anil and 5 others requested to connect.',
       description: 'View all connection request',
       timestamp: this.getTimeAgo('2025-05-15T03:00:00Z'),
-      isRead: false
+      isRead: false,
+      isExpanded: false
     },
     {
       type: 'event',
       title: '2 New events are now live on the platform.',
       description: 'Upcoming event: Digital Learning Week. Save your spot!',
       timestamp: this.getTimeAgo('2025-05-12T04:00:00Z'),
-      isRead: true
+      isRead: true,
+      isExpanded: false
     },
     {
       type: "discuss",
       title: "Anil and 7 others liked your post.",
       description: "View all likes on your post.",
       timestamp: this.getTimeAgo('2025-02-15T04:00:00Z'),
-      isRead: false
+      isRead: false,
+      isExpanded: false
     }
   ]
 
@@ -58,7 +66,8 @@ export class AllNotificationsComponent implements OnInit {
       title: '3 Courses recommended by Neha Agarwal.',
       description: 'You have a new message from your mentor.',
       timestamp: this.getTimeAgo('2025-05-15T04:51:00Z'),
-      isRead: false
+      isRead: false,
+      courses: ["do_11389554191936716817", "do_11431661045542092814", "do_1143018621876715521203"]
     },
     {
       type: 'network',
@@ -157,5 +166,9 @@ export class AllNotificationsComponent implements OnInit {
     this.dynamicTabIndex = this.currentTab
     console.log('currentTab', this.currentTab)
     this.getNotificationsObject()
+  }
+
+  action(notification: any) {
+    notification.isExpanded = !notification.isExpanded
   }
 }
