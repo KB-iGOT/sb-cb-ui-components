4820e27c400dcb1bcc67c07b3074b7e5ba260534
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2025-08-06 18:52:47 +0530

    Tagging users under comments section in the TOC page

--- sb-cb-ui-discussion-v2/library/sunbird-cb/discussion-v2/src/lib/_common/comment-card/comment-card.component.ts
+++ sb-cb-ui-discussion-v2/library/sunbird-cb/discussion-v2/src/lib/_common/comment-card/comment-card.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core'
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core'
 import { NsDiscussionV2 } from '../../_model/discussion-v2.model'
 import { CommentsService } from '../../_services/comments.service'
 import { ConfigurationsService } from '@sunbird-cb/utils-v2'
@@ -55,13 +55,32 @@ export class CommentCardComponent implements OnInit, OnChanges {
     // "Others"
   ]
 
+  // Mention tracking
+  @ViewChild('description') descriptionTextarea!: ElementRef<HTMLTextAreaElement>
+  showMentionDropdown = false;
+  mentionDropdownPosition = { top: 0, left: 0 };
+  rootOrgId = ''
+  mentionUsers: any[] = [];
+  isMentioning = false;
+  mentionSearchText = '';
+  mentionStartPosition = 0;
+  activeMentionIndex = 0;
+  isLoadingUsers = false;
+  mentionedUsers: any[] = []; // Track mentioned users for API
+
   constructor(
     public commentSvc: CommentsService,
     private configSvc: ConfigurationsService,
     private _snackBar: MatSnackBar,
     private ref: ChangeDetectorRef,
     private dialog: MatDialog
-  ) { }
+  ) {
+    if (this.configSvc
+      && this.configSvc.userProfile
+      && this.configSvc.userProfile.rootOrgId) {
+      this.rootOrgId = this.configSvc.userProfile.rootOrgId
+    }
+  }
 
   ngOnInit() {
     this.loogedInUserProfile = this.configSvc.userProfile
@@ -84,7 +103,7 @@ export class CommentCardComponent implements OnInit, OnChanges {
   }
 
   newComment(event: any) {
-    
+
     if (event.response && event.response.comment && event.response.comment.commentId) {
       this.loading = true
       this.emptySearch()
@@ -114,21 +133,21 @@ export class CommentCardComponent implements OnInit, OnChanges {
   getListOfReplies() {
     let reveseReplayDataCopy = [...this.replyDataCopy]
     reveseReplayDataCopy.reverse()
-    let ids:any = reveseReplayDataCopy.slice(0,10)
+    let ids: any = reveseReplayDataCopy.slice(0, 10)
     this.commentSvc.getListOfCommentsById(ids).subscribe(res => {
       this.loading = false
       if (res.result && res.result.comments.length) {
         let taggedUsersList = res.result.taggedUsers
-        this.tagUserData = {...this.tagUserData,..._.keyBy(taggedUsersList, 'user_id')}
+        this.tagUserData = { ...this.tagUserData, ..._.keyBy(taggedUsersList, 'user_id') }
         const reply = res.result.comments
         // parrent comment id is user for sencond level comments only
-        const replayModified = reply.map((replayData: any) => ({...replayData, parentCommentId: this.comment.parentCommentId || this.comment.commentId}))
+        const replayModified = reply.map((replayData: any) => ({ ...replayData, parentCommentId: this.comment.parentCommentId || this.comment.commentId }))
         this.fetchedReplyData = [...replayModified,]
-        this.newReply.emit({ response: [], type: 'reply', replyDataCopy:this.replyDataCopy, replyData: this.fetchedReplyData })
-        if(res.result && res.result.users && res.result.users.length) {
-                  let commentUsersDataObj = res.result.users
-                  this.commentUsersData = {...this.commentUsersData,..._.keyBy(commentUsersDataObj, 'user_id')}
-        } 
+        this.newReply.emit({ response: [], type: 'reply', replyDataCopy: this.replyDataCopy, replyData: this.fetchedReplyData })
+        if (res.result && res.result.users && res.result.users.length) {
+          let commentUsersDataObj = res.result.users
+          this.commentUsersData = { ...this.commentUsersData, ..._.keyBy(commentUsersDataObj, 'user_id') }
+        }
       }
     },
       () => {
@@ -171,7 +190,7 @@ export class CommentCardComponent implements OnInit, OnChanges {
     //     this.likeUnlikeCommentApi('like', event.commentId)
     //   }
     // })
-    if(this.userLikedComments.includes(event.commentId)) {
+    if (this.userLikedComments.includes(event.commentId)) {
       this.likeUnlikeCommentApi('dislike', event.commentId)
     } else {
       this.likeUnlikeCommentApi('like', event.commentId)
@@ -258,29 +277,32 @@ export class CommentCardComponent implements OnInit, OnChanges {
 
   }
   deleteCommentMethod(comment: any) {
-    const index = this.replyData.findIndex((commentId: any) => commentId === comment.commentId);
-    const parentInderx = this.replyData.findIndex((ele: any) => ele.commentId === comment.commentId);
+    const index = this.replyData.findIndex((commentId: any) => commentId === comment.commentId)
+    const parentInderx = this.replyData.findIndex((ele: any) => ele.commentId === comment.commentId)
     if (index !== -1) {
-      this.replyData.splice(index, 1);
-      this.replyData = this.replyData.slice();
+      this.replyData.splice(index, 1)
+      this.replyData = this.replyData.slice()
       this.replyDataCopy = this.replyData
     }
-    if(parentInderx !== -1) {
-      this.replyParendtData.splice(parentInderx, 1);
-      this.replyParendtData = this.replyParendtData.slice();
-    }      
-    this.newReply.emit({ response: [], type: 'reply', replyDataCopy:this.replyDataCopy, replyData: this.replyParendtData , onlyDeleteCount: true})
-    this.commentSvc.deleteComment(comment.commentId, this.commentSvc.entityType, this.commentSvc.entityId, this.commentSvc.workflow , comment.parentCommentId || '').subscribe((_res: any) => {
+    if (parentInderx !== -1) {
+      this.replyParendtData.splice(parentInderx, 1)
+      this.replyParendtData = this.replyParendtData.slice()
+    }
+    this.newReply.emit({ response: [], type: 'reply', replyDataCopy: this.replyDataCopy, replyData: this.replyParendtData, onlyDeleteCount: true })
+    this.commentSvc.deleteComment(comment.commentId, this.commentSvc.entityType, this.commentSvc.entityId, this.commentSvc.workflow, comment.parentCommentId || '').subscribe((_res: any) => {
       comment.status = 'inactive'
       this._snackBar.open('Comment deleted successfully')
-    }, (_err: any)=> {
+    }, (_err: any) => {
       this._snackBar.open('Something went wrong! please try again later.')
     })
   }
 
-  toggelEdit(commentData:any) {
+  toggelEdit(commentData: any) {
     this.editCommentData = {}
-    this.editCommentData = {...commentData}
+    this.editCommentData = { ...commentData }
+    console.log(this.editCommentData)
+    this.mentionedUsers = this.editCommentData.mentionedUsers
+    console.log(this.mentionedUsers)
     this.isEditMode = true
     this.replayCommentsCount = this.replayCommentsCount + 10
   }
@@ -296,8 +318,34 @@ export class CommentCardComponent implements OnInit, OnChanges {
     this.editCommentData['comment'] = text
   }
 
+  getMentionedUsers(text: string) {
+    const mentions = this.extractMentions(text)
+    const users: any = []
+    mentions.forEach(mention => {
+      const username = mention.substring(1)
+      const user = this.mentionedUsers.find(u => u.userName === username)
+      if (user && user.userId) {
+        users.push({
+          userId: user.userId,
+          userName: user.userName
+        })
+      }
+    })
+    return users
+  }
+
+  extractMentions(text: string): string[] {
+    if (!text) {
+      return []
+    }
+    const mentionRegex = /@\w+/g
+    return text.match(mentionRegex) || []
+  }
+
   updateComment() {
-    let requestData = {
+    const mentions = this.getMentionedUsers(this.editCommentData.comment)
+    debugger
+    let requestData: any = {
       "commentTreeId": this.commentSvc.commentTreeId,
       "commentId": this.comment.commentId,
       "commentData": {
@@ -306,6 +354,9 @@ export class CommentCardComponent implements OnInit, OnChanges {
         "commentSource": this.editCommentData.commentSource,
         "taggedUsers": this.editCommentData.taggedUsers
       }
+    }
+    if (mentions.length > 0) {
+      requestData.commentData['mentionedUsers'] = mentions
     }
     this.commentSvc.updateComment(requestData).subscribe((_res: any) => {
       this.isEditMode = false
@@ -348,10 +399,10 @@ export class CommentCardComponent implements OnInit, OnChanges {
         const replayModified = reply.map((replayData: any) => ({ ...replayData, parentCommentId: this.comment.parentCommentId || this.comment.commentId }))
         this.fetchedReplyData = [...this.fetchedReplyData, ...replayModified]
         this.newReply.emit({ response: [], type: 'reply', replyDataCopy: this.replyDataCopy, replyData: this.fetchedReplyData })
-        if(res.result && res.result.users && res.result.users.length) {
+        if (res.result && res.result.users && res.result.users.length) {
           let commentUsersDataObj = res.result.users
-          this.commentUsersData = {...this.commentUsersData,..._.keyBy(commentUsersDataObj, 'user_id')}
-} 
+          this.commentUsersData = { ...this.commentUsersData, ..._.keyBy(commentUsersDataObj, 'user_id') }
+        }
         this.loading = false
       }
     },
@@ -375,5 +426,152 @@ export class CommentCardComponent implements OnInit, OnChanges {
   }
   emptySearch() {
     this.commentSvc.emptyCommentSearch().subscribe((_res: any) => { })
+  }
+
+  handleInput(event: Event): void {
+    console.log('Input event:', event)
+    if (this.descriptionTextarea?.nativeElement) {
+      this.checkForMention()
+    }
+  }
+
+  checkForMention(): void {
+    if (!this.descriptionTextarea?.nativeElement) {
+      return
+    }
+    const textarea = this.descriptionTextarea.nativeElement
+    const text = textarea.value
+    const cursorPosition = textarea.selectionStart
+
+    // Find the start of the current word
+    let startPos = cursorPosition
+    while (startPos > 0 && text[startPos - 1] !== ' ' && text[startPos - 1] !== '\n') {
+      startPos--
+    }
+
+    // Check if the current word starts with @
+    if (startPos < cursorPosition && text[startPos] === '@') {
+      const searchText = text.substring(startPos + 1, cursorPosition)
+
+      if (!this.isMentioning) {
+        this.isMentioning = true
+        this.mentionStartPosition = startPos
+        this.activeMentionIndex = 0
+      }
+
+      if (this.mentionSearchText !== searchText) {
+        this.mentionSearchText = searchText
+        this.searchUsers(searchText)
+      }
+
+      // Position the dropdown below the @ symbol
+      this.positionMentionDropdown()
+    } else {
+      this.closeMentionDropdown()
+    }
+  }
+
+  positionMentionDropdown(): void {
+    const textarea = this.descriptionTextarea.nativeElement
+
+    // Create a temporary element to calculate position
+    const tempElement = document.createElement('div')
+    tempElement.style.position = 'absolute'
+    tempElement.style.visibility = 'hidden'
+    tempElement.style.whiteSpace = 'pre-wrap'
+    tempElement.style.width = textarea.clientWidth + 'px'
+    tempElement.style.font = window.getComputedStyle(textarea).font
+    tempElement.style.lineHeight = window.getComputedStyle(textarea).lineHeight
+    tempElement.style.padding = window.getComputedStyle(textarea).padding
+
+    // Add text up to cursor
+    tempElement.textContent = textarea.value.substring(0, this.mentionStartPosition)
+
+    // Add a span where the @ is to get position
+    const atSpan = document.createElement('span')
+    atSpan.textContent = '@'
+    tempElement.appendChild(atSpan)
+
+    document.body.appendChild(tempElement)
+
+    // Get position of the @ character
+    const rect = atSpan.getBoundingClientRect()
+    const textareaRect = textarea.getBoundingClientRect()
+
+    // Calculate position relative to textarea
+    this.mentionDropdownPosition = {
+      top: rect.bottom - textareaRect.top + 5,
+      left: rect.left - textareaRect.left
+    }
+
+    // Clean up
+    document.body.removeChild(tempElement)
+
+    // Show dropdown
+    this.showMentionDropdown = true
+  }
+
+  closeMentionDropdown(): void {
+    this.showMentionDropdown = false
+    this.isMentioning = false
+    this.mentionSearchText = ''
+  }
+
+  searchUsers(query: string): void {
+    this.isLoadingUsers = true
+
+    // Call your API to search users
+    this.commentSvc.searchUsers(query, this.rootOrgId).subscribe(
+      (data: any) => {
+        this.isLoadingUsers = false
+        if (data.result && data.result.response) {
+          const users = data.result.response.content
+          this.mentionUsers = users.map((user: any) => ({
+            userId: user.userId,
+            userName: `${user.userName}`,
+          }))
+        } else {
+          this.mentionUsers = []
+        }
+      },
+      (error) => {
+        console.error('Error fetching users for mention:', error)
+        this.isLoadingUsers = false
+        this.mentionUsers = []
+      }
+    )
+  }
+
+  selectMention(user: any): void {
+    const textarea = this.descriptionTextarea.nativeElement
+    const originalValue = textarea.value
+    // Add the selected user to mentioned users list for tracking
+    if (!this.mentionedUsers.find(_user => _user && _user.userId === user.userId)) {
+      this.mentionedUsers.push({
+        userId: user.userId,
+        userName: user.userName
+      })
+      // Replace the @query with @username
+      const beforeMention = originalValue.substring(0, this.mentionStartPosition)
+      const afterMention = originalValue.substring(textarea.selectionStart)
+      const mentionText = `@${user.userName}`
+
+      // Set new value
+      const newValue = beforeMention + mentionText + afterMention
+
+      // Update form control value
+      this.editCommentData.comment = newValue
+      textarea.value = newValue
+      // Set cursor position after the inserted mention
+      setTimeout(() => {
+        textarea.focus()
+        const newCursorPosition = this.mentionStartPosition + mentionText.length
+        textarea.setSelectionRange(newCursorPosition, newCursorPosition)
+      })
+      // Close dropdown
+      this.closeMentionDropdown()
+    } else {
+      //this._snackBar.open(`You have already tagged ${user.userName}`)
+    }
   }
 }
