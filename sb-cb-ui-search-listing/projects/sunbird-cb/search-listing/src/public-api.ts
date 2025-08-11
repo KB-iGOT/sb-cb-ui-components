397f419329056a975328bb54d775a2d6487e333d
Sohith Kanala <kanala.sohith@tarento.com>
2025-08-11 15:32:16 +0530

    add serarch input to exports

--- sb-cb-ui-search-listing/projects/sunbird-cb/search-listing/src/public-api.ts
+++ sb-cb-ui-search-listing/projects/sunbird-cb/search-listing/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from "./lib/_services/search-listing.service";
 export * from "./lib/search-listing.component";
 export * from "./lib/search-listing.module";
+export * from "./lib/_components/search-input-home/search-input-home.component";
