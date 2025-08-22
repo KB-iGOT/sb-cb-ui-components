880953fa39abfc3babf05def64617a9f54c58854
Sohith Kanala <kanala.sohith@tarento.com>
2025-08-22 17:01:32 +0530

    remove search-listing

--- sb-cb-ui-search-listing/projects/sunbird-cb/search-listing/src/lib/_components/skeleton-loader-content/skeleton-loader-content.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "ws-app-skeleton-loader-content",
-  templateUrl: "./skeleton-loader-content.component.html",
-  styleUrls: ["./skeleton-loader-content.component.scss"],
-})
-export class SkeletonLoaderContentComponent {}
--- sb-cb-ui-search-listing/projects/sunbird-cb/search-listing/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of search-listing
- */
-
-export * from "./lib/_services/search-listing.service";
-export * from "./lib/search-listing.component";
-export * from "./lib/search-listing.module";
-export * from "./lib/_components/search-input-home/search-input-home.component";
