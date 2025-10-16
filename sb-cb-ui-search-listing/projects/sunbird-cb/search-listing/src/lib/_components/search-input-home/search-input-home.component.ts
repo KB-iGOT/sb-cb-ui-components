61855204b7589f520b66480f0b29f447b23c5414
Sohith Kanala <kanala.sohith@tarento.com>
2025-10-16 17:59:38 +0530

    search fixes

--- sb-cb-ui-search-listing/projects/sunbird-cb/search-listing/src/lib/_components/search-input-home/search-input-home.component.ts
+++ sb-cb-ui-search-listing/projects/sunbird-cb/search-listing/src/lib/_components/search-input-home/search-input-home.component.ts
@@ -168,17 +168,20 @@ export class SearchInputHomeComponent implements OnInit, OnChanges {
     } else {
       this.disableMenu = false;
     }
-
-    //Only allow communities for mdo_leader and mdo moderator in MDO
     if (this.searchConfig?.applicationName === SearchListingConfig.ApplicationNames.MDOPortal) {
       const userRoles = this.configSvc?.userRoles as Set<string>;
       if (this.searchConfig.searchCategories) {
-        if (!userRoles.has("mdo_leader") && !userRoles.has("community_moderator")) {
+        const hasMdoAdmin = userRoles.has("mdo_admin");
+        const hasMdoLeader = userRoles.has("mdo_leader");
+        const hasCommunityModerator = userRoles.has("community_moderator");
+        if ((hasMdoAdmin && hasCommunityModerator) || (hasMdoLeader && hasCommunityModerator) || hasMdoLeader) {
+        } else if (hasMdoAdmin) {
           this.searchConfig.searchCategories = this.searchConfig?.searchCategories.filter(category => category?.value !== SearchCategory.Communities);
-        }
-        if (userRoles.has("community_moderator")) {
+        } else if (hasCommunityModerator) {
           this.searchConfig.searchCategories = this.searchConfig?.searchCategories.filter(category => category?.value === SearchCategory.Communities);
           this.searchConfig.searchInputConfig.defaultSearchCategory = SearchCategory.Communities;
+        } else {
+          this.searchConfig.searchCategories = this.searchConfig?.searchCategories.filter(category => category?.value !== SearchCategory.Communities);
         }
       }
     }
