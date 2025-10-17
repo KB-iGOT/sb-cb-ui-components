455d4c899ce8cdc14cfb9ad4403a9af151e5e19b
Sohith Kanala <kanala.sohith@tarento.com>
2025-10-17 12:17:04 +0530

    search fixes

--- sb-cb-ui-search-listing/projects/sunbird-cb/search-listing/src/lib/_components/training-plans-card/training-plans-card.component.ts
+++ sb-cb-ui-search-listing/projects/sunbird-cb/search-listing/src/lib/_components/training-plans-card/training-plans-card.component.ts
@@ -23,7 +23,7 @@ export class TrainingPlansCardComponent implements OnInit {
     if (
       (this.userProfile?.firstName === plan?.createdByName ||
         this.userProfile?.userRoles?.has("mdo_leader")) &&
-      plan?.status?.toLowerCase() !== "retire"
+      plan?.status?.toLowerCase() 
     ) {
       url = `/app/training-plan/update-plan/${plan?.id}`;
     } else {
