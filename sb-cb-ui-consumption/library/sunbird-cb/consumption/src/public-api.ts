a37beab23a7e63b0a7985a29f46b81bde7797872
Salonitarento <saloni.chhaperwal@tarento.com>
2026-03-12 11:10:21 +0530

    added reusable card and modal component for badge and updated v to 4.… (#787)
    * added reusable card and modal component for badge and updated v to 4.8.35
    
    * updated the package v to 0.0.2

--- sb-cb-ui-consumption/library/sunbird-cb/consumption/src/public-api.ts
+++ sb-cb-ui-consumption/library/sunbird-cb/consumption/src/public-api.ts
@@ -136,6 +136,8 @@ export * from './lib/_common/create-request/dialogs/add-authors/add-authors.comp
 export * from './lib/_common/filters/filters.module'
 export * from './lib/_common/filters/filter-by/filter-by.component'
 export * from './lib/_common/filters/search-sort-input/search-sort-input.component'
+export * from './lib/_common/cards/badge-modal/badge-modal.component'
+export * from './lib/_common/cards/badge-stats/badge-stats.component'
 
 export * from './lib/_common/competencies/competencies.module'
 export * from './lib/_common/competencies/competency-list/competency-list.component'
