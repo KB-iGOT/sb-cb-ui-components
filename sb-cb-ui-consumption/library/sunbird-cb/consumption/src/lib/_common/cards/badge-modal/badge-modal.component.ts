21bdbf80dc8b20515672bfc2bb458bca079ee41f
Salonitarento <saloni.chhaperwal@tarento.com>
2026-03-25 16:01:50 +0530

    Kb10345 gamific badges (#842)
    * added reusable card and modal component for badge and updated v to 4.8.35
    
    * updated the package v to 0.0.2
    
    * added slider for badge and updated v to 0.0.35
    
    * updated package version of consumption package to 0.0.6
    
    * updated package v in toc library to 0.0.35
    
    * added tooltip on badges, conditional slider for curated program and updated v
    
    * added view modal and download
    
    * fixed % sign in content card issue, and also removed badge icon if not there and updated version
    
    * fixed kb-13593 bug
    
    * updated toc package version

--- sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/_common/cards/badge-modal/badge-modal.component.ts
+++ sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/_common/cards/badge-modal/badge-modal.component.ts
@@ -2,13 +2,16 @@ import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core'
 import { PipePublicURLModule } from '@sunbird-cb/utils-v2'
 import { BadgeService } from '../../../_services/badge.service'
 import { ConfigurationsService } from '@sunbird-cb/utils-v2'
+import jsPDF from 'jspdf'
+import { MatIconModule } from '@angular/material/icon'
+import { MatMenuModule } from '@angular/material/menu'
 
 @Component({
   selector: 'sb-uic-app-badge-modal',
   templateUrl: './badge-modal.component.html',
   styleUrls: ['./badge-modal.component.scss'],
   standalone: true,
-  imports: [PipePublicURLModule]
+  imports: [PipePublicURLModule, MatIconModule, MatMenuModule]
 })
 export class BadgeModalComponent implements OnInit {
   @Input() badgeData: any
@@ -16,10 +19,43 @@ export class BadgeModalComponent implements OnInit {
   }
   @Output() close = new EventEmitter<void>()
 
-  constructor(private badgeService: BadgeService, private configSvc: ConfigurationsService) {}
+  constructor(private badgeService: BadgeService, private configSvc: ConfigurationsService) { }
 
- 
-  downloadBadge(badgeData: any) {
+  downloadBadgePng(badgeData: any) {
+    const payload = {
+      request: {
+        userId: this.configSvc?.userProfile?.userId,
+        courseId: this.badgeData?.courseId,
+        badgeId: this.badgeData?.badgeId,
+      },
+    }
+    this.badgeService.generateBadge(payload).subscribe((res: any) => {
+      const dataUrl = res?.result?.printUri
+
+      const img = new Image()
+      img.src = dataUrl
+
+      img.onload = () => {
+        const canvas = document.createElement('canvas')
+        canvas.width = img.width
+        canvas.height = img.height
+
+        const ctx = canvas.getContext('2d')
+        if (ctx) {
+          ctx.drawImage(img, 0, 0)
+
+          const png = canvas.toDataURL('image/png')
+
+          const a = document.createElement('a')
+          a.href = png
+          a.download = 'badge.png'
+          a.click()
+        }
+      }
+    })
+  }
+  
+    downloadBadgeSvg(badgeData: any) {
     console.log("downloadBadge")
     const payload = {
       request: {
@@ -29,21 +65,56 @@ export class BadgeModalComponent implements OnInit {
       },
     }
    console.log('payload========', payload, badgeData, this.configSvc)
-    this.badgeService.generateBadge(payload).subscribe({
-      next: (response: Blob) => {
-        const blob = new Blob([response], { type: 'application/pdf' })
-        const url = window.URL.createObjectURL(blob)
+   this.badgeService.generateBadge(payload).subscribe({
+    next: (res: any) => {
+      const dataUrl = res?.result?.printUri  
 
-        const a = document.createElement('a')
-        a.href = url
-        a.download = 'badge.pdf'
-        a.click()
+      const a = document.createElement('a')
+      a.href = dataUrl
+a.download = 'badge.svg'
+     
+      document.body.appendChild(a)
+      a.click()
+      a.remove()
+    },
+    error: (err) => {
+      console.error('Download failed', err)
+    },
+  })
+  }
+  downloadBadgePdf(badgeData: any) {
+    const payload = {
+      request: {
+        userId: this.configSvc?.userProfile?.userId,
+        courseId: this.badgeData?.courseId,
+        badgeId: this.badgeData?.badgeId,
+      },
+    }
+    this.badgeService.generateBadge(payload).subscribe((res: any) => {
+      const dataUrl = res?.result?.printUri
 
-        window.URL.revokeObjectURL(url)
-      },
-      error: (err) => {
-        console.error('Download failed', err)
-      },
+      const img = new Image()
+      img.src = dataUrl
+
+      img.onload = () => {
+        const canvas = document.createElement('canvas')
+        canvas.width = img.width || 1920
+        canvas.height = img.height || 1080
+
+        const ctx = canvas.getContext('2d')
+        if (ctx) {
+          ctx.drawImage(img, 0, 0)
+
+          const imgData = canvas.toDataURL('image/png')
+
+          const pdf = new jsPDF('landscape', 'px', 'a4')
+          const w = pdf.internal.pageSize.getWidth()
+          const h = pdf.internal.pageSize.getHeight()
+
+          pdf.addImage(imgData, 'PNG', 0, 0, w, h)
+          pdf.save('badge.pdf')
+        }
+      }
     })
   }
 getUserId(): string {
