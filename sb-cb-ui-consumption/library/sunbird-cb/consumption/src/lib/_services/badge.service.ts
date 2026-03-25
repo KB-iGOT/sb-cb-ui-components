0253920895669dc611525397a206d24f844161fa
SaloniTarento <saloni.chhaperwal@tarento.com>
2026-03-25 12:25:22 +0530

    fixed % sign in content card issue, and also removed badge icon if not there and updated version

---
+++ sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/_services/badge.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BadgeService {
+  private apiUrl =
+    'https://portal.dev.karmayogibharat.net/apis/proxies/v8/badge/dynamic/v1/generate'
+
+  constructor(private http: HttpClient) {}
+
+  generateBadge(data: any): Observable<Blob> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+    console.log('data========', data)
+    return this.http.post(this.apiUrl, data, {
+      headers,
+      responseType: 'blob', // important for download
+      withCredentials: true, // for cookie
+    })
+  }
+}
