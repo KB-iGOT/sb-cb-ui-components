5e86a19e17eb15a64d6d35f1587916bf6140426f
SaloniTarento <saloni.chhaperwal@tarento.com>
2026-03-26 13:15:04 +0530

    fixed bug 13599 and 13630

--- sb-cb-ui-toc/library/sunbird-cb/toc/src/lib/components/app-toc-home-v2/app-toc-home-v2.component.ts
+++ sb-cb-ui-toc/library/sunbird-cb/toc/src/lib/components/app-toc-home-v2/app-toc-home-v2.component.ts
@@ -3337,11 +3337,11 @@ export class AppTocHomeV2Component implements OnInit, OnDestroy, AfterViewChecke
     }
     const badge = badgeDetails[0]
     // If badgeEarningDateEnabled is false, don't show badge
-    if (!badge.badgeEarningDateEnabled) {
-      return false
+    if (!badge?.badgeEarningDateEnabled) {
+      return true
     }
     // If badgeEarningDateEnabled is true, show badge only if badgeEarningDateTime has passed
-    if (badge.badgeEarningDateEnabled && badge.badgeEarningDateTime) {
+    if (badge?.badgeEarningDateEnabled && badge?.badgeEarningDateTime) {
       console.log(badge.badgeEarningDateTime, Date.now())
       return badge.badgeEarningDateTime > Date.now()
     }
