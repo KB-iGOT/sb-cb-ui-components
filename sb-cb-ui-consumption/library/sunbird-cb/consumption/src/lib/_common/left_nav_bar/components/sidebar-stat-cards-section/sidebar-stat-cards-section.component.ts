1353d087f671ad9bada33cffb2f00d036888c7af
mansurskTarento <mansur.shaik@tarento.com>
2026-06-05 15:56:25 +0530

    wip KB-12710: UI/UX Homepage Revamp - Web adding dark theme class

--- sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/_common/left_nav_bar/components/sidebar-stat-cards-section/sidebar-stat-cards-section.component.ts
+++ sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/_common/left_nav_bar/components/sidebar-stat-cards-section/sidebar-stat-cards-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, computed, signal, ChangeDetectionStrategy } from '@angular/core'
+import { Component, Input, Output, EventEmitter, computed, ChangeDetectionStrategy } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button'
@@ -6,7 +6,6 @@ import { MatExpansionModule } from '@angular/material/expansion'
 import { MatRippleModule } from '@angular/material/core'
 import { TranslateModule, TranslateService } from '@ngx-translate/core'
 import { StatCardsSectionConfig, StatCardItem } from '../../models/sidebar.models'
-import { DEFAULTS } from '../../constants/sidebar.constants'
 import { MultilingualTranslationsService } from '../../../../_services/multilingual-translations.service'
 import { SkeletonLoaderLibModule } from '../../../skeleton-loader-lib/skeleton-loader-lib.module'
 
@@ -58,6 +57,8 @@ export class SidebarStatCardsSectionComponent {
    */
   @Input({ required: true }) showContent!: boolean
 
+  @Output() itemClicked = new EventEmitter<string>()
+
   constructor(
     private translate: TranslateService,
     private langtranslations: MultilingualTranslationsService
@@ -79,41 +80,24 @@ export class SidebarStatCardsSectionComponent {
   }
 
   /**
-   * Signal to track if "View All" is expanded
-   */
-  isExpanded = signal<boolean>(false);
-
-  /**
-   * Computed signal for visible items based on maxItemsVisible and expansion state
+   * Computed signal for all items (no limit applied)
    */
   visibleItems = computed(() => {
-    if (!this.section?.items) return []
-
-    if (this.section.maxItemsVisible === null) return this.section.items
-
-    const maxItems = this.section.maxItemsVisible ?? DEFAULTS.MAX_ITEMS_VISIBLE
-    const shouldLimit = this.section.showViewAll && !this.isExpanded() && maxItems
-
-    return shouldLimit
-      ? this.section.items.slice(0, maxItems)
-      : this.section.items
+    return this.section?.items ?? []
   });
 
   /**
    * Computed signal to check if "View All" button should be shown
    */
   shouldShowViewAll = computed(() => {
-    if (!this.section?.showViewAll || !this.section?.items) return false
-
-    const maxItems = this.section.maxItemsVisible ?? DEFAULTS.MAX_ITEMS_VISIBLE
-    return this.section.items.length > maxItems
+    return this.section?.showViewAll ?? false
   });
 
   /**
-   * Toggle view all expansion
+   * Handle View All click - emit event to parent
    */
-  toggleViewAll(): void {
-    this.isExpanded.update(state => !state)
+  onViewAllClick(): void {
+    this.itemClicked.emit(this.section.viewAllKey ?? '')
   }
 
   /**
