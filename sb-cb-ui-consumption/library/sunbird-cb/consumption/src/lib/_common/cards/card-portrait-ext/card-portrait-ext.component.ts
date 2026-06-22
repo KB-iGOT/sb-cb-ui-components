e5b531e02a251b8f1ddb2d4debdeabe249e05bc9
Bharath Ravi <117885618+bharathravi-in@users.noreply.github.com>
2026-06-22 13:33:04 +0530

    Temp role based (#958)
    * KB-13929 progress of child resources does not show correctly intermittently
    
    * updated to angular 20
    
    * oackage updated
    
    * added new language
    
    * added new language (#939)
    
    * update for library changes
    
    * viewer bug fix
    
    * Cbrelease 4.8.36 (#942)
    
    * changes in the language
    
    * changes in domain config
    
    * update for survey form
    
    * changes in domain config for api integration
    
    * utills-v2 package change
    
    * update for collection packages
    
    * update for ang changes
    
    * update for ang 20
    
    * TOC page changes
    
    * update for ang 20 fix
    
    * changes in the consumption for the card and content library
    
    ---------
    
    Co-authored-by: Sohith Kanala <kanala.sohith@tarento.com>
    Co-authored-by: abhishek-K-P <73942654+abhishek-K-P@users.noreply.github.com>
    Co-authored-by: Abhishek K P <abhishek.koira@tarentoc.om>
    Co-authored-by: vishnu <vishnubansaltarento>
    Co-authored-by: Afzal84 <mohd.afzal@tarento.com>

--- sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/_common/cards/card-portrait-ext/card-portrait-ext.component.ts
+++ sb-cb-ui-consumption/library/sunbird-cb/consumption/src/lib/_common/cards/card-portrait-ext/card-portrait-ext.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 import { NsCardContent } from '../../../_models/card-content.model'
 import { MatSnackBar } from '@angular/material/snack-bar'
-import { ConfigurationsService, EventService } from '@sunbird-cb/utils-v2'
+import { ConfigurationsService, DomainConfService, EventService } from '@sunbird-cb/utils-v2'
 import * as _ from "lodash"
 import { TranslateService } from '@ngx-translate/core'
 import { MultilingualTranslationsService } from '../../../_services/multilingual-translations.service'
@@ -34,6 +34,7 @@ export class CardPortraitExtComponent implements OnInit {
     private translate: TranslateService,
     private langtranslations: MultilingualTranslationsService,
     private configSvc: ConfigurationsService,
+    private domainConfSvc: DomainConfService,
     private contSvc: WidgetContentLibService,) {
     this.langtranslations.languageSelectedObservable.subscribe(() => {
       if (localStorage.getItem('websiteLanguage')) {
@@ -42,6 +43,10 @@ export class CardPortraitExtComponent implements OnInit {
         this.translate.use(lang)
       }
     })
+  }
+
+  isCardElementEnabled(key: string): boolean {
+    return this.domainConfSvc.isConfigEnabled('components.cards', key)
   }
 
   ngOnInit() {
