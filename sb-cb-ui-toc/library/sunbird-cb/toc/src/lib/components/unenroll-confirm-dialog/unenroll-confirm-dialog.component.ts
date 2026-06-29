69a9640f94703821a83d505bcd04d233347cbbeb
venkykandagaddala <venky.kandagaddala@gmail.com>
2026-06-29 11:55:21 +0530

    Un-enroll form

--- sb-cb-ui-toc/library/sunbird-cb/toc/src/lib/components/unenroll-confirm-dialog/unenroll-confirm-dialog.component.ts
+++ sb-cb-ui-toc/library/sunbird-cb/toc/src/lib/components/unenroll-confirm-dialog/unenroll-confirm-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core'
+import { FormArray, FormControl, FormGroup } from '@angular/forms'
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
 
 export interface UnenrollDialogData {
@@ -13,6 +14,18 @@ export interface UnenrollDialogData {
 })
 export class UnenrollConfirmDialogComponent {
   understood = false
+  showFeedbackForm = false
+  feedbackReasons = [
+    'Content is not relevant to my role',
+    'Course is too difficult / too easy',
+    'Not enough time to complete',
+    'Poor content quality',
+    'Enrolled by mistake',
+  ]
+  feedbackForm = new FormGroup({
+    reasons: new FormArray(this.feedbackReasons.map(() => new FormControl(false))),
+    comments: new FormControl(''),
+  })
 
   constructor(
     public dialogRef: MatDialogRef<UnenrollConfirmDialogComponent>,
@@ -25,6 +38,28 @@ export class UnenrollConfirmDialogComponent {
 
   onContinue(): void {
     if (this.understood) {
+      this.showFeedbackForm = true
+    }
+  }
+
+  onBack(): void {
+    this.showFeedbackForm = false
+  }
+
+  hasSelectedFeedbackReason(): boolean {
+    return (this.feedbackForm.get('reasons') as FormArray).value.some(Boolean)
+  }
+
+  onConfirmUnenroll(): void {
+    if (this.hasSelectedFeedbackReason()) {
+      const selectedReasonFlags = (this.feedbackForm.get('reasons') as FormArray).value
+      const selectedReasons = this.feedbackReasons.filter((_, index) => selectedReasonFlags[index])
+      const feedbackPayload = {
+        selectedReasons,
+        comments: this.feedbackForm.get('comments')?.value || '',
+      }
+
+      console.log('Un-enroll feedback:', feedbackPayload)
       this.dialogRef.close(true)
     }
   }
